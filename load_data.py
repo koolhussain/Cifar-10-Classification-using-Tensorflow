7923a012550e2c65f10271d09690c1097fa74d58
Ahmad Hussain <ahmad.hussain91@gmail.com>
2018-06-12 19:04:01 +0530

    Add files via upload

--- load_data.py
+++ load_data.py
@@ -1,74 +1,49 @@
-import download
+import urllib
 import os
-import pickle
 import numpy as np
-import matplotlib.pyplot as plt
+import pickle
+from tflearn.data_utils import to_categorical
 
-url = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
-path = os.getcwd()
+def LoadBatch(fpath):
+    with open(fpath, 'rb') as f:
+      d = pickle.load(f, encoding='latin1')
+    data = d["data"]
+    labels = d["labels"]
+    return data, labels
 
-def download_data():
-    if os.path.isdir(os.path.join(path,"cifar-10-batches-py")):
-        print("File already Exists")
+
+def LoadCifarData(filepath='cifar-10-python.tar.gz',
+                  extract_dir='cifar-10-batches-py/', 
+                  one_hot=False):
+  X_train = []
+  Y_train = []
+  for i in range(1, 6):
+    fpath = os.path.join(extract_dir, 'data_batch_' + str(i))
+    data, labels = LoadBatch(fpath)
+    if i == 1:
+      X_train = data
+      Y_train = labels
     else:
-        download.download(url, path, kind='tar.gz', progressbar = True, replace = False, verbose = False)
+      X_train = np.concatenate([X_train, data], axis=0)
+      Y_train = np.concatenate([Y_train, labels], axis=0)
 
-def get_filepath():
-    return os.path.join(path,"cifar-10-batches-py")
+  fpath = os.path.join(extract_dir, 'test_batch')
+  X_test, Y_test = LoadBatch(fpath)
 
-def unpickle(filename):
-    with open(filename, 'rb') as f:
-        data = pickle.load(f, encoding='latin1')
-    img_data = data["data"]
-    img_labels = data["labels"]
-    return img_data, img_labels
+  X_train = np.dstack((X_train[:, :1024], X_train[:, 1024:2048],
+                       X_train[:, 2048:])) / 255.
+  X_train = np.reshape(X_train, [-1, 32, 32, 3])
+  X_test = np.dstack((X_test[:, :1024], X_test[:, 1024:2048],
+                      X_test[:, 2048:])) / 255.
+  X_test = np.reshape(X_test, [-1, 32, 32, 3])
 
-def convert_images(data):
-    X_image = []
-    for im in data:
-        im_r = im[0:1024]
-        im_g = im[1024:2048]
-        im_b = im[2048:]
+  if one_hot:
+    Y_train = to_categorical(Y_train,10)
+    Y_test = to_categorical(Y_test,10)
 
-        img = np.dstack((im_r,im_g,im_b))
-        img_r = np.reshape(img,[32,32,3])
-        X_image.append(img_r)
-    return X_image
-    
-def load_data():
-    X_train = []
-    Y_train = []
-    filepath = get_filepath()
-    for i in range(1,6):
-        filename = os.path.join(filepath,'data_batch_' + str(i))
-        img_data, img_labels = unpickle(filename)
-        if i == 1:
-            X_train = img_data
-            Y_train = img_labels
-        else:
-            X_train = np.concatenate([X_train, img_data], axis=0)
-            Y_train = np.concatenate([Y_train, img_labels], axis=0)
-
-    X_train = convert_images(X_train)
-    
-    filename = os.path.join(filepath,"test_batch")
-    X_test, Y_test = unpickle(filename)
-
-    X_test = convert_images(X_test)
-
-    return (X_train, Y_train), (X_test, Y_test)
-
-def plotting():
-    (X_train, Y_train), (X_test, Y_test) = load_data()
-    print(X_train[0].shape)
-    plt.imshow(X_train[1000])
-    plt.show()
-
-if __name__ == "__main__":
-    plotting()
+  return (X_train, Y_train), (X_test, Y_test)
 
 
 
-
-    
-load_data()
+if __name__ == '__main__':
+  main()
